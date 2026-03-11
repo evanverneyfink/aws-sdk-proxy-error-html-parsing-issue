badd5176c067387421ee0e572218b08ce5ff0c30
Evan Verney-Fink <9777656+evanverneyfink@users.noreply.github.com>
2026-03-11 14:42:56 -0400

    Add sample project reproducing issue

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Text;
+using Amazon.Runtime.Internal.Transform;
+using Amazon.Runtime.Internal.Util;
+
+const string html = "<html><body>test<div>test</div>test</body></html>";
+
+var htmlBytes = Encoding.UTF8.GetBytes(html);
+var htmlStream = new MemoryStream(htmlBytes);
+
+var jsonReader = new StreamingUtf8JsonReader(new MemoryStream(htmlBytes));
+var jsonUnmarshallerContext = new JsonUnmarshallerContext(htmlStream, true, new TestWebResponseData(htmlStream), true);
+var jsonUnmarshaller = new JsonErrorResponseUnmarshaller();
+
+var buffer = new byte[htmlBytes.Length];
+jsonUnmarshallerContext.Stream.ReadExactly(buffer, 0, buffer.Length);
+
+Console.WriteLine("Unmarshalling...");
+
+jsonUnmarshaller.Unmarshall(jsonUnmarshallerContext, ref jsonReader);
+
+Console.WriteLine("Unmarshalling complete");
+
+class TestWebResponseData(Stream stream) : IWebResponseData
+{
+    public long ContentLength => stream.Length;
+
+    public IHttpResponseBody ResponseBody { get; } = new TestHttpResponseBody(stream);
+
+    public string ContentType => "text/html";
+
+    public HttpStatusCode StatusCode => HttpStatusCode.ServiceUnavailable;
+
+    public bool IsSuccessStatusCode => false;
+
+    public string[] GetHeaderNames() => [];
+
+    public string? GetHeaderValue(string headerName) => null;
+
+    public bool IsHeaderPresent(string headerName) => false;
+}
+
+class TestHttpResponseBody(Stream stream) : IHttpResponseBody
+{
+    public void Dispose() { }
+
+    public Stream OpenResponse() => stream;
+
+    public Task<Stream> OpenResponseAsync() => Task.FromResult(stream);
+}
